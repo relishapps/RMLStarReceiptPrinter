57e767248fd140b184cd5126edf7f19757c3c197
Pawel Decowski <pawel.decowski@gmail.com>
2015-08-10 18:33:10 +0100

    Initial commit

---
+++ Example/RMLStarReceiptPrinter/RMLViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMLViewController.h
+//  RMLStarReceiptPrinter
+//
+//  Created by Pawel Decowski on 08/10/2015.
+//  Copyright (c) 2015 Pawel Decowski. All rights reserved.
+//
+
+@import UIKit;
+
+@interface RMLViewController : UIViewController
+
+@end
---
+++ Example/RMLStarReceiptPrinter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RMLStarReceiptPrinter
+//
+//  Created by Pawel Decowski on 08/10/2015.
+//  Copyright (c) 2015 Pawel Decowski. All rights reserved.
+//
+
+@import UIKit;
+#import "RMLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMLAppDelegate class]));
+    }
+}
